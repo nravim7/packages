cde6d940aaac5c8962cb5741cfafd068fc025877
Ravi N <your.email@example.com>
2024-06-18 03:02:38 +0530

    Packages

---
+++ src/dev/ravi/main.java
@@ -0,0 +1,11 @@
+package dev.ravi;
+
+
+import com.abc.first.*;
+
+public class main {
+    public static void main(String[] args) {
+        Item item = new Item("Burger");
+        System.out.println(item);
+    }
+}
